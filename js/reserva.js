1d802325d686955b4325186bb95eabd6fafe9e59
Henoc Elías Jorge Ugarte <henoceliasjorgeugarte@gmail.com>
2025-05-10 02:26:59 -0500

    Added backend for reserva-form
    Added process for storing reserva-form data in an external database table.

--- js/reserva.js
+++ js/reserva.js
@@ -15,10 +15,6 @@ function reserva(e) {
 
         const nombre = document.getElementById("nombre").value;
 
-
-
-        form.reset();
-
         document.querySelector(".form-container").style.display = 'none';
         mensajeDiv.style.display = "block";
 
@@ -30,6 +26,12 @@ function reserva(e) {
             document.querySelector(".form-container").style.display = 'none';
             mensajeDiv.style.display = 'none';
         }, 1500);
+
+        //para enviar formulario
+        const data = Object.fromEntries(new FormData(form).entries());
+        guardarForm(data);
+
+        form.reset();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -37,7 +39,10 @@ document.addEventListener("DOMContentLoaded", function () {
         botonReserva.addEventListener("click", () => mostrarForm('.form-container'));
     }
     
-    form.addEventListener("submit", (e) => reserva(e));
+    form.addEventListener("submit", function (e) {
+        reserva(e);
+    });
+
     linkReserva.addEventListener('click', () => mostrarForm('.form-container'))
 
     document.getElementById("cerrar-reserva").addEventListener("click", () => {
@@ -46,3 +51,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+const SUPABASE_URL = 'https://hogwabktijnpthrpvoxe.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvZ3dhYmt0aWpucHRocnB2b3hlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4NTQ3MDMsImV4cCI6MjA2MjQzMDcwM30.aqkxIxn2-bVnveFh0kWLdn3gzJlWmrQkBZjNib0F5RI';
+
+async function guardarForm(data) {
+    const res = await fetch(`${SUPABASE_URL}/rest/v1/forms_prereg`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'apikey': SUPABASE_KEY,
+            'Authorization': `Bearer ${SUPABASE_KEY}`
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (res.ok) {
+        console.log("Formulario enviado correctamente")
+    } else {
+        const error = await res.text();
+        console.error("Error: ", error)
+        console.log("Hubo un error al enviar el formulario");
+    }
+}
